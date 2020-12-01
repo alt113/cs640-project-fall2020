e838eaafab9f8248ff06f406ac3f70aac3fc4966
temilaj <temilaj@hotmail.com>
2020-12-01 00:56:55 -0500

    clean up

--- train.py
+++ train.py
@@ -13,7 +13,6 @@ from tensorflow.keras.optimizers import SGD
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
